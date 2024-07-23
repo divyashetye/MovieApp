b302c24456ba63eaa36a670d24979d07bf1d18e8
Divya Shetye <divya.shetye5901@gmail.com>
2024-07-23 20:40:38 +0530

    Second commit

--- src/components/Details/details.jsx
+++ src/components/Details/details.jsx
@@ -1,15 +1,35 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import { useParams } from 'react-router';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './details.scss'
 
 const Details = () => {
     const { id } = useParams();
-    const [moviedata, setMoviedata] = useState({})
+    const [moviedata, setMoviedata] = useState({});
+    const firstRender = useRef(true);
+    const [timer, setTimer] = useState(3);
 
     useEffect(() => {
-        getdetails();
+        if (firstRender.current) {
+            getdetails();
+            firstRender.current = false
+        }
     }, [id]);
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setTimer((prevTimer) => { 
+                if (prevTimer > 0) {
+                    return prevTimer - 1;
+                } else {
+                    clearInterval(intervalId);
+                    return 0;
+                }
+            });
+        }, 1000);
+
+        return () => clearInterval(intervalId); // Clear interval on component unmount
+    }, []);
 
     const getdetails = async () => {
         try {
@@ -24,52 +44,60 @@ const Details = () => {
     }
 
     return (
-        <div className='container-fluid stylebox'>
-            <div className='row'>
-                <div className='col-xl-4 leftdiv'>
-                    {
-                        moviedata &&
-                        <img src={`https://image.tmdb.org/t/p/w500/${moviedata.poster_path}`} alt='poster' />
-                    }
+        <div className="timer">
+            {timer > 0 ? (
+                <h4>Loading in {timer} seconds...</h4>
+            ) : (
+                <div className='container-fluid stylebox'>
+
+                    <div className='row'>
+                        <div className='col-xl-4 leftdiv'>
+                            {
+                                moviedata &&
+                                <img src={`https://image.tmdb.org/t/p/w500/${moviedata.poster_path}`} alt='poster' />
+                            }
+                        </div>
+
+                        <div className='col-xl-7 rightdiv'>
+                            {
+                                moviedata &&
+                                <>
+                                    <h2>{moviedata.original_title}</h2>
+                                    <div className='overview'>
+                                        <h4><span>Release Date : </span>{moviedata.release_date}</h4>
+
+                                        {moviedata.origin_country && (
+                                            <h4><span>Origin Country : </span>{moviedata.origin_country[0]}</h4>
+                                        )}
+
+
+                                        <h4><span>Language : </span>{moviedata.original_language}</h4>
+
+                                        {moviedata.genres && (
+                                            <h4><span>Genres : </span>{moviedata.genres.map(genre => genre.name).join(', ')}</h4>
+                                        )}
+
+                                        {moviedata.production_companies &&
+                                            <h4><span>Production Companies : </span>{moviedata.production_companies.map(prod => prod.name).join(', ')}</h4>
+                                        }
+
+                                        <h4><span>Status : </span>{moviedata.status}</h4>
+
+                                        {moviedata.tagline && (
+                                            <h4><span>Tagline : </span>{moviedata.tagline}</h4>
+                                        )}
+
+                                        <h4><span>Overview : </span></h4>
+                                        <p>{moviedata.overview}</p>
+
+                                    </div>
+
+                                </>
+                            }
+                        </div>
+                    </div>
                 </div>
-
-                <div className='col-xl-7 rightdiv'>
-                    {
-                        moviedata &&
-                        <>
-                            <h2>{moviedata.original_title}</h2>
-                            <div className='overview'>
-                                <h4><span>Release Date : </span>{moviedata.release_date}</h4>
-
-                                {moviedata.origin_country && (
-                                    <h4><span>Origin Country : </span>{moviedata.origin_country[0]}</h4>
-                                )}
-
-
-                                <h4><span>Language : </span>{moviedata.original_language}</h4>
-
-                                {moviedata.genres && (
-                                    <h4><span>Genres : </span>{moviedata.genres.map(genre => genre.name).join(', ')}</h4>
-                                )}
-
-                                {moviedata.production_companies &&
-                                    <h4><span>Production Companies : </span>{moviedata.production_companies.map(prod => prod.name).join(', ')}</h4>
-                                }
-
-                                <h4><span>Status : </span>{moviedata.status}</h4>
-
-                                {moviedata.tagline && (
-                                <h4><span>Tagline : </span>{moviedata.tagline}</h4>
-                                )}
-
-                                <h4><span>Overview : </span></h4>
-                                <p>{moviedata.overview}</p>
-                                
-                            </div>
-                        </>
-                    }
-                </div>
-            </div>
+            )}
         </div>
     )
 }
--- src/components/Home/home.jsx
+++ src/components/Home/home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import './home.scss';
 import TextField from '@mui/material/TextField';
 import { useNavigate } from 'react-router-dom';
@@ -8,10 +8,14 @@ const Home = () => {
   const [movies, setMovies] = useState([]);
   const [searchmovie, setSearchmovie] = useState([])
   const navigate = useNavigate();
+  const initialRender = useRef(true);
 
   //API to get the movies
   useEffect(() => {
-    getmovie();
+    if (initialRender.current) {
+      getmovie();
+      initialRender.current = false;  // Mark initial render as done
+    }
   }, [])
 
   const getmovie = async () => {
@@ -45,6 +49,15 @@ const Home = () => {
 
   return (
     <div className='container home'>
+      {/* <header>
+      <img src='https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.underconsideration.com%2Fbrandnew%2Farchives%2Fnew_logo_for_the_movie_database.php&psig=AOvVaw12Nkw8tV3eF3ij1CV2BpVT&ust=1720779428877000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCOjmjYbhnocDFQAAAAAdAAAAABAE'></img>
+        <div className='main'>
+        <div>Home</div>
+        <div>Categories</div>
+        <div>Contact Us</div>
+        </div>
+      </header> */}
+
       <div className='searchdiv'>
         <h2>Get Started With Your Journey To Cinematic Adventures...</h2>
         <TextField className='searchmovie' id="outlined-basic" label="Search movie" variant="outlined" 
